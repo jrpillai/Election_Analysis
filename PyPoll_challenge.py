bab703d678fbbb93f6a4eef7bfdfc806a0c0d165
jrpillai <99375785+jrpillai@users.noreply.github.com>
2022-03-05 11:36:36 -0600

    to see error

--- PyPoll_challenge.py
+++ PyPoll_challenge.py
@@ -5,6 +5,8 @@
 import csv
 import os
 
+# to see upload issue
+
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
